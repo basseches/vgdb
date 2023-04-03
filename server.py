e12c7781af757c81c401ee48ae4dbb6108f343d7
Ivy <imb2139@columbia.edu>
2023-04-03 08:49:10 +0000

    add platform querying

--- server.py
+++ server.py
@@ -43,30 +43,30 @@ engine = create_engine(DATABASEURI)
 
 @app.before_request
 def before_request():
-	"""
-	This function is run at the beginning of every web request 
-	(every time you enter an address in the web browser).
-	We use it to setup a database connection that can be used throughout the request.
+    """
+    This function is run at the beginning of every web request 
+    (every time you enter an address in the web browser).
+    We use it to setup a database connection that can be used throughout the request.
 
-	The variable g is globally accessible.
-	"""
-	try:
-		g.conn = engine.connect()
-	except:
-		print("uh oh, problem connecting to database")
-		import traceback; traceback.print_exc()
-		g.conn = None
+    The variable g is globally accessible.
+    """
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback; traceback.print_exc()
+        g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-	"""
-	At the end of the web request, this makes sure to close the database connection.
-	If you don't, the database could run out of memory!
-	"""
-	try:
-		g.conn.close()
-	except Exception as e:
-		pass
+    """
+    At the end of the web request, this makes sure to close the database connection.
+    If you don't, the database could run out of memory!
+    """
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
 
 
 #
@@ -84,59 +84,59 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
-	"""
-	request is a special object that Flask provides to access web request information:
+    """
+    request is a special object that Flask provides to access web request information:
 
-	request.method:   "GET" or "POST"
-	request.form:     if the browser submitted a form, this contains the data in the form
-	request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+    request.method:   "GET" or "POST"
+    request.form:     if the browser submitted a form, this contains the data in the form
+    request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-	See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
-	"""
-	#
-	# example of a database query
-	#
-	select_query = "select s.game_id, s.title, s.popularity_rating, ROW_NUMBER() OVER (ORDER BY popularity_rating desc) from (select distinct game_id, title, popularity_rating from game) as s LIMIT 3"
-	cursor = g.conn.execute(text(select_query))
-	names = []
-	for result in cursor:
-		names.append([result[1], result[2], result[3], result[0]])
-	cursor.close()
+    See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
+    """
+    #
+    # example of a database query
+    #
+    select_query = "select s.game_id, s.title, s.popularity_rating, ROW_NUMBER() OVER (ORDER BY popularity_rating desc) from (select distinct game_id, title, popularity_rating from game) as s LIMIT 3"
+    cursor = g.conn.execute(text(select_query))
+    names = []
+    for result in cursor:
+        names.append([result[1], result[2], result[3], result[0]])
+    cursor.close()
 
-	#
-	# Flask uses Jinja templates, which is an extension to HTML where you can
-	# pass data to a template and dynamically generate HTML based on the data
-	# (you can think of it as simple PHP)
-	# documentation: https://realpython.com/primer-on-jinja-templating/
-	#
-	# You can see an example template in templates/index.html
-	#
-	# context are the variables that are passed to the template.
-	# for example, "data" key in the context variable defined below will be 
-	# accessible as a variable in index.html:
-	#
-	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-	#     <div>{{data}}</div>
-	#     
-	#     # creates a <div> tag for each element in data
-	#     # will print: 
-	#     #
-	#     #   <div>grace hopper</div>
-	#     #   <div>alan turing</div>
-	#     #   <div>ada lovelace</div>
-	#     #
-	#     {% for n in data %}
-	#     <div>{{n}}</div>
-	#     {% endfor %}
-	#
-	context = dict(data = names)
+    #
+    # Flask uses Jinja templates, which is an extension to HTML where you can
+    # pass data to a template and dynamically generate HTML based on the data
+    # (you can think of it as simple PHP)
+    # documentation: https://realpython.com/primer-on-jinja-templating/
+    #
+    # You can see an example template in templates/index.html
+    #
+    # context are the variables that are passed to the template.
+    # for example, "data" key in the context variable defined below will be 
+    # accessible as a variable in index.html:
+    #
+    #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+    #     <div>{{data}}</div>
+    #     
+    #     # creates a <div> tag for each element in data
+    #     # will print: 
+    #     #
+    #     #   <div>grace hopper</div>
+    #     #   <div>alan turing</div>
+    #     #   <div>ada lovelace</div>
+    #     #
+    #     {% for n in data %}
+    #     <div>{{n}}</div>
+    #     {% endfor %}
+    #
+    context = dict(data = names)
 
 
-	#
-	# render_template looks in the templates/ folder for files.
-	# for example, the below file reads template/index.html
-	#
-	return render_template("index.html", **context)
+    #
+    # render_template looks in the templates/ folder for files.
+    # for example, the below file reads template/index.html
+    #
+    return render_template("index.html", **context)
 
 @app.route('/addgame', methods=['GET'])
 def addgame_page():
@@ -145,7 +145,7 @@ def addgame_page():
     cursor = g.conn.execute(text(mode_query))
     mode = []
     for result in cursor:
-    	mode.append([result[0], result[1]])
+        mode.append([result[0], result[1]])
     #genre query
     genre_query = "select title from genre"
     cursor = g.conn.execute(text(genre_query))
@@ -223,113 +223,147 @@ def addgame():
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
-	# accessing form inputs from user
-	name = request.form['name']
-	
-	# passing params in for each variable into query
-	params = {}
-	params["new_name"] = name
-	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-	g.conn.commit()
-	return redirect('/')
+    # accessing form inputs from user
+    name = request.form['name']
+    
+    # passing params in for each variable into query
+    params = {}
+    params["new_name"] = name
+    g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
+    g.conn.commit()
+    return redirect('/')
 
 #------------------------------------------------------------------------------
 # QUERYING
 
 @app.route('/query')
 def query():
-	return render_template("query.html")
+    return render_template("query.html")
 
 @app.route('/query/budget')
 def budget():
-	# getting the minimum and maximum prices for our range sliders
-	query = "SELECT TO_CHAR(MIN(current_price), 'FM999999990'), " \
-		"TO_CHAR(MAX(current_price), 'FM999999990') FROM game"
-	cursor = g.conn.execute(text(query))
-	names = []
-	for result in cursor:
-		names.append([result[0], result[1]])
-	cursor.close()
-	context = dict(data = names)
-	return render_template("budget.html", **context)
+    # getting the minimum and maximum prices for our range sliders
+    query = "SELECT TO_CHAR(MIN(current_price), 'FM999999990'), " \
+        "TO_CHAR(MAX(current_price), 'FM999999990') FROM game"
+    cursor = g.conn.execute(text(query))
+    names = []
+    for result in cursor:
+        names.append([result[0], result[1]])
+    cursor.close()
+    context = dict(data = names)
+    return render_template("budget.html", **context)
+
+@app.route('/query/platform')
+def platform():
+    platform_query = "SELECT title FROM platform"
+    cursor = g.conn.execute(text(platform_query))
+
+    platform  = []
+    for result in cursor:
+        platform.append(result[0])
+    cursor.close()
+    context = dict(data = platform)
+    return render_template("queryplatform.html", **context)
 
 @app.route('/query/budget', methods=['POST'])
 def budget_post():
-	# accessing form inputs from user
-	min_price = request.form['min']
-	max_price = request.form['max']
+    # accessing form inputs from user
+    min_price = request.form['min']
+    max_price = request.form['max']
 
-	params = {}
-	params["min_price"] = min_price
-	params["max_price"] = max_price
+    params = {}
+    params["min_price"] = min_price
+    params["max_price"] = max_price
 
-	query = "SELECT g.game_id, g.title, g.platform, " \
-		"TO_CHAR(g.current_price, 'FM999999990.00') FROM game g " \
-		"WHERE current_price >= :min_price AND current_price <= :max_price " \
-		"ORDER BY current_price ASC"
-	cursor = g.conn.execute(text(query), params)
+    query = "SELECT g.game_id, g.title, g.platform, " \
+        "TO_CHAR(g.current_price, 'FM999999990.00') FROM game g " \
+        "WHERE current_price >= :min_price AND current_price <= :max_price " \
+        "ORDER BY current_price ASC"
+    cursor = g.conn.execute(text(query), params)
 
-	names = []
-	for result in cursor:
-		names.append([result[0], result[1], result[2], result[3]])
-	cursor.close()
-	context = dict(data = names)
+    names = []
+    for result in cursor:
+        names.append([result[0], result[1], result[2], result[3]])
+    cursor.close()
+    context = dict(data = names)
 
-	return render_template("results.html", **context)
+    return render_template("results.html", **context)
+
+@app.route('/query/platform', methods=['POST'])
+def platform_post():
+    # accessing form inputs from user
+    params = {}
+    params["platform"] = request.form['platform']
+
+    query = "SELECT g.game_id, g.title, g.platform FROM game g " \
+        "WHERE g.platform = :platform"
+    cursor = g.conn.execute(text(query), params)
+
+    names = []
+    for result in cursor:
+        names.append([result[0], result[1], result[2]])
+    cursor.close()
+    context = dict(data = names)
+
+    return render_template("results.html", **context)
 
 #------------------------------------------------------------------------------
 
 @app.route('/game/<game>')
 def game(game = None):
-	if game == None:
-		return render_template("notfound.html")
+    if game == None:
+        return render_template("notfound.html")
 
-	params = {}
-	params["game"] = game
-	query = "SELECT g.title, g.description, g.popularity_rating, " \
-			"g.esrb_rating, TO_CHAR(g.release_price, 'FM999999990.00'), " \
-			"TO_CHAR(g.current_price, 'FM999999990.00'), g.game_mode, " \
-			"g.genre, g.franchise, g.platform, g.dev_leader, d.title, " \
-			"p.title FROM (game g LEFT OUTER JOIN company d ON " \
-			"g.developer = d.company_id) LEFT OUTER JOIN company p ON " \
-			"g.publisher = p.company_id WHERE g.game_id = :game"
-	cursor = g.conn.execute(text(query), params)
-	names = []
-	for result in cursor:
-		names.append([thing for thing in result])
-	cursor.close()
-	context = dict(data = names)
-	return render_template("game.html", **context)
+    params = {}
+    params["game"] = game
+    query = "SELECT g.title, g.description, g.popularity_rating, " \
+            "g.esrb_rating, TO_CHAR(g.release_price, 'FM999999990.00'), " \
+            "TO_CHAR(g.current_price, 'FM999999990.00'), g.game_mode, " \
+            "g.genre, g.franchise, g.platform, g.dev_leader, d.title, " \
+            "p.title FROM (game g LEFT OUTER JOIN company d ON " \
+            "g.developer = d.company_id) LEFT OUTER JOIN company p ON " \
+            "g.publisher = p.company_id WHERE g.game_id = :game"
+    cursor = g.conn.execute(text(query), params)
+    names = []
+    for result in cursor:
+        names.append([thing for thing in result])
+    cursor.close()
+
+    if not names:
+        return render_template("notfound.html")
+
+    context = dict(data = names)
+    return render_template("game.html", **context)
 
 @app.route('/login')
 def login():
-	abort(401)
-	this_is_never_executed()
+    abort(401)
+    this_is_never_executed()
 
 
 if __name__ == "__main__":
-	import click
+    import click
 
-	@click.command()
-	@click.option('--debug', is_flag=True)
-	@click.option('--threaded', is_flag=True)
-	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8111, type=int)
-	def run(debug, threaded, host, port):
-		"""
-		This function handles command line parameters.
-		Run the server using:
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
+        This function handles command line parameters.
+        Run the server using:
 
-			python server.py
+            python server.py
 
-		Show the help text using:
+        Show the help text using:
 
-			python server.py --help
+            python server.py --help
 
-		"""
+        """
 
-		HOST, PORT = host, port
-		print("running on %s:%d" % (HOST, PORT))
-		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 run()
